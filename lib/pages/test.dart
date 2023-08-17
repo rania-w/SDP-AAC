ffe6fb87e2371eca081efed079723b3df6435f24
rania-w <rania.weiss@stu.ibu.edu.ba>
2023-08-17 23:29:57 +0200

    test page

---
+++ lib/pages/test.dart
@@ -0,0 +1,39 @@
+import 'package:aac/components/cards/custom_card.dart';
+import 'package:aac/components/cards/image_card.dart';
+import 'package:aac/components/cards/row_card.dart';
+import 'package:aac/components/custom_button.dart';
+import 'package:aac/components/search.dart';
+import 'package:aac/components/sentence_button.dart';
+import 'package:aac/components/word_title.dart';
+import 'package:flutter/material.dart';
+
+class Test extends StatefulWidget {
+  const Test({super.key});
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("AAC"),
+          backgroundColor: Color(0xFFFFECEC),
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                CustomButton(text: "custom button"),
+                CustomCard(title: "custom card", imageAsset: 'resources/images/pedro.jpg'),
+                ImageCard(imageAsset: 'resources/images/pedro.jpg'),
+                RowCard(title: "row card", imageAsset: 'resources/images/pedro.jpg'),
+                Search(),
+                SentenceButton(sentence: "sentence"),
+                WordTitle(title: "title")
+              ],
+            )));
+  }
+}
