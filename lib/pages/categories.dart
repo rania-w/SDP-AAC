0f93a1d82012fdc42e9cf5c5b73830cd8d980321
rania-w <rania.weiss@stu.ibu.edu.ba>
2023-08-17 23:28:05 +0200

    bottomnav

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:aac/components/row_card.dart';
+import 'package:aac/components/cards/row_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:aac/objects/category.dart';
@@ -30,21 +30,23 @@ class _CategoriesState extends State<Categories> {
         words: [Word(word: "word 1", image: "resources/images/frog.jpeg")])
   ];
 
+  int bottomNavBarIndex = 1;
+
   @override
   Widget build(BuildContext context) {
     // const String i = '/resources/images/frog.jpeg';
     return Scaffold(
       appBar: AppBar(
         title: const Text("AAC"),
-        backgroundColor: Colors.pink,
+        backgroundColor: Color(0xFFFFECEC),
       ),
       body: Padding(
         padding: const EdgeInsets.all(24),
         child: Column(
           children: [
             Search(),
-            //TODO: iter
-            RowCard(title: "title", imageAsset: "imageAsset")
+            for (var i in categories)
+              RowCard(title: i.title, imageAsset: i.image)
           ],
         ),
         //check how this looks on a phone - dreadful, better on tablet
@@ -53,7 +55,30 @@ class _CategoriesState extends State<Categories> {
         onPressed: () {},
         splashColor: Color(0xFF9ead7f),
         backgroundColor: Color(0xFFc1ccac),
-        child: const Icon(Icons.add, color: Color(0xFF393E41),),
+        child: const Icon(
+          Icons.add,
+          color: Color(0xFF393E41),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: "settings"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "build"),
+        ],
+        currentIndex: bottomNavBarIndex,
+        onTap: (index) {
+          setState(() {
+            bottomNavBarIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color(0xFFFFFCFC).withOpacity(0.8),
+        iconSize: 32,
+        // TODO moze bolje ovaj navbar al zasad radi svoje
+        selectedItemColor: Color(0xFF393E41),
+
       ),
     );
   }
