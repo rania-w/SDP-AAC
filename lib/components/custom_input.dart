3213a08b96ec56a7ccf4c906f4a33a378a3f48a2
rania-w <rania.weiss@stu.ibu.edu.ba>
2023-08-20 16:33:48 +0200

    autocomplete input widget

---
+++ lib/components/custom_input.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomInput extends StatelessWidget {
+  const CustomInput({super.key});
+
+  static const List<String> _kOptions = <String>[
+    'aardvark',
+    'bobcat',
+    'chameleon',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 14),
+      child: Autocomplete<String>(
+        optionsBuilder: (TextEditingValue textEditingValue) {
+          if (textEditingValue.text == '') {
+            return const Iterable<String>.empty();
+          }
+          return _kOptions.where((String option) {
+            return option.contains(textEditingValue.text.toLowerCase());
+          });
+        },
+        onSelected: (String selection) {
+          debugPrint('You just selected $selection');
+        },
+      ),
+    );
+  }
+}
