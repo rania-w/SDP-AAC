1d3018180616139d81a3abb5c7dab8066e96e706
rania-w <rania.weiss@stu.ibu.edu.ba>
2023-08-31 14:50:41 +0200

    constants used through app

--- lib/constants.dart
+++ lib/constants.dart
@@ -13,3 +13,21 @@ const textInputDecoration = InputDecoration(
 );
 
 const black = Color(0xff393E41);
+const white = Color(0xfffcfcfc);
+const primary = Color(0xffFFECEC);
+const primaryF = Color(0xffDAB9B9);
+const green = Color(0xffC1CCAC);
+const greenF = Color(0xff9EAD7F);
+const grey = Color(0xff8E9999);
+const imageAsset = 'lib/resources/images/frog.jpeg';
+
+var appBar = AppBar(
+  backgroundColor: primary,
+  iconTheme: IconThemeData(color: black),
+  title: Text(
+    "AAC",
+    style: textStyle.copyWith(fontFamily: 'Medium'),
+  ),
+);
+
+const textStyle = TextStyle(color: black);
