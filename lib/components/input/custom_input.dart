a8eb2a9c9ea7dee1851d656fdf1b043e2bbb4b6d
rania-w <rania.weiss@stu.ibu.edu.ba>
2023-09-02 20:33:10 +0200

    sentence building start

--- lib/components/input/custom_input.dart
+++ lib/components/input/custom_input.dart
@@ -1,30 +1,45 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:aac/constants.dart';
+import 'package:aac/objects/word.dart';
+import 'package:aac/services/boxes.dart';
 import 'package:flutter/material.dart';
 
-class CustomInput extends StatelessWidget {
-  const CustomInput({super.key});
+/// zasto ne nadje stvari kako treba??
 
-  static const List<String> _kOptions = <String>[
-    'aardvark',
-    'bobcat',
-    'chameleon',
-  ];
+class CustomInput extends StatefulWidget {
+  final Function(String) onSelected;
+
+  const CustomInput({super.key, required this.onSelected});
 
   @override
+  State<CustomInput> createState() => _CustomInputState();
+}
+
+class _CustomInputState extends State<CustomInput> {
+  @override
   Widget build(BuildContext context) {
+    List<String> wordList = [];
+    for (var i in boxWord.values) {
+      wordList.add(i.word);
+    }
     return Container(
       margin: EdgeInsets.only(bottom: 14),
+      decoration: BoxDecoration(
+          color: white,
+          border: Border.all(width: 1.5, color: black),
+          borderRadius: BorderRadius.circular(10)),
       child: Autocomplete<String>(
         optionsBuilder: (TextEditingValue textEditingValue) {
           if (textEditingValue.text == '') {
             return const Iterable<String>.empty();
           }
-          return _kOptions.where((String option) {
+          return wordList.where((String option) {
             return option.contains(textEditingValue.text.toLowerCase());
           });
         },
         onSelected: (String selection) {
           debugPrint('You just selected $selection');
+          widget.onSelected(selection);
         },
       ),
     );
--- lib/pages/sentence_building.dart
+++ lib/pages/sentence_building.dart
@@ -1,8 +1,11 @@
 // ignore_for_file: prefer_const_constructors
+
 import 'package:aac/components/buttons/custom_button.dart';
 import 'package:aac/components/input/custom_input.dart';
 import 'package:aac/constants.dart';
 import 'package:flutter/material.dart';
+
+/// kako napraviti da ikonica ostane pri desnoj strani
 
 class SentenceBuilding extends StatefulWidget {
   const SentenceBuilding({super.key});
@@ -12,11 +15,59 @@ class SentenceBuilding extends StatefulWidget {
 }
 
 class _SentenceBuildingState extends State<SentenceBuilding> {
+  List<String> received = [];
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        CustomInput(),
+        CustomInput(
+          onSelected: (selected) {
+            setState(() {
+              received.add(selected);
+            });
+          },
+        ),
+        Container(
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              border: Border.all(color: grey)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              for (var i in received)
+                Container(
+                  decoration: BoxDecoration(
+                      color: primary,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: primaryF, width: 1.5)),
+                  margin: EdgeInsets.symmetric(horizontal: 4),
+                  padding: EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Text(i),
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            received.remove(i);
+                          });
+                        },
+                        icon: Icon(
+                          Icons.close,
+                          color: black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              IconButton(
+                  onPressed: () {
+                    //tts
+                  },
+                  icon: Icon(Icons.volume_up))
+            ],
+          ),
+        ),
         CustomButton(
           text: "Save sentence",
           onPressed: () {},
