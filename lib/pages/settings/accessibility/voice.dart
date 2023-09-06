e7f565c893dc538ac1456185fc6239e77c5096b8
rania-w <rania.weiss@stu.ibu.edu.ba>
2023-09-06 13:13:37 +0200

    voice page

--- lib/pages/settings/accessibility/voice.dart
+++ lib/pages/settings/accessibility/voice.dart
@@ -1,4 +1,7 @@
+import 'package:aac/components/buttons/custom_button.dart';
+import 'package:aac/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:aac/services/tts.dart';
 
 class VoiceSettings extends StatefulWidget {
   const VoiceSettings({super.key});
@@ -8,7 +11,11 @@ class VoiceSettings extends StatefulWidget {
 }
 
 class _VoiceSettingsState extends State<VoiceSettings> {
-  double volume = 1.0;
+  double volume = 1;
+  double pitch = 1;
+  double rate = 1;
+
+  var languages = tts.getLanguages;
 
   @override
   Widget build(BuildContext context) {
@@ -22,14 +29,16 @@ class _VoiceSettingsState extends State<VoiceSettings> {
               margin: const EdgeInsets.only(left: 10),
               child: Row(
                 children: [
-                  const SizedBox(
+                  SizedBox(
                     width: 80,
                     child: Text(
-                      "Volume",
-                      style: TextStyle(fontSize: 17),
+                      "Jačina zvuka",
+                      style: paragraph.copyWith(fontWeight: FontWeight.bold),
                     ),
                   ),
                   Slider(
+                    activeColor: green,
+                    inactiveColor: grey.withOpacity(0.2),
                     min: 0.0,
                     max: 1.0,
                     value: volume,
@@ -43,10 +52,111 @@ class _VoiceSettingsState extends State<VoiceSettings> {
                     margin: const EdgeInsets.only(left: 10),
                     child: Text(
                         double.parse((volume).toStringAsFixed(2)).toString(),
-                        style: const TextStyle(fontSize: 17)),
+                        style: paragraph.copyWith(fontWeight: FontWeight.bold)),
                   )
                 ],
               ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 10),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 80,
+                    child: Text(
+                      "Visina",
+                      style: paragraph.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Slider(
+                    activeColor: green,
+                    inactiveColor: grey.withOpacity(0.2),
+                    min: 0.5,
+                    max: 2.0,
+                    value: pitch,
+                    onChanged: (value) {
+                      setState(() {
+                        pitch = value;
+                      });
+                    },
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10),
+                    child: Text(
+                        double.parse((pitch).toStringAsFixed(2)).toString(),
+                        style: paragraph.copyWith(fontWeight: FontWeight.bold)),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 10),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 80,
+                    child: Text(
+                      "Brzina",
+                      style: paragraph.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Slider(
+                    activeColor: green,
+                    inactiveColor: grey.withOpacity(0.2),
+                    min: 0.01,
+                    max: 1.0,
+                    value: rate,
+                    onChanged: (value) {
+                      setState(() {
+                        rate = value;
+                      });
+                    },
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10),
+                    child: Text(
+                        double.parse((rate).toStringAsFixed(2)).toString(),
+                        style: paragraph.copyWith(fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomButton(
+              defaultColor: primary,
+              text: 'Test',
+              onPressed: () async {
+                await tts.setPitch(pitch);
+                await tts.setLanguage('hr-HR');
+                await tts.setVolume(volume);
+                await tts.setSpeechRate(rate);
+                await tts.speak('Ovo je testna izjava');
+              },
+              focusColor: primaryF,
+            ),
+            Row(
+              children: [
+                CustomButton(
+                  onPressed: () {
+                    //reset to before state
+                    ttsConf(rate, volume, pitch, 'hr-HR');
+                  },
+                  text: 'Nazad',
+                  defaultColor: white,
+                  focusColor: greenF,
+                ),
+                CustomButton(
+                  onPressed: () {
+                    //save changes
+                    ttsConf(rate, volume, pitch, 'hr-HR');
+                  },
+                  text: 'Spasi promjene',
+                  defaultColor: green,
+                  focusColor: greenF,
+                ),
+              ],
             ),
           ],
         ),
