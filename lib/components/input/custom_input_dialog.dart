a401fd3c28cc7a6b12e1155ff70b157c14829005
rania-w <rania.weiss@stu.ibu.edu.ba>
2023-09-12 20:53:21 +0200

    change nickname

---
+++ lib/components/input/custom_input_dialog.dart
@@ -0,0 +1,61 @@
+import 'package:aac/constants.dart';
+import 'package:aac/objects/user.dart';
+import 'package:aac/services/boxes.dart';
+import 'package:flutter/material.dart';
+
+class CustomInputDialog extends StatefulWidget {
+  const CustomInputDialog({super.key});
+
+  @override
+  State<CustomInputDialog> createState() => _CustomInputDialogState();
+}
+
+class _CustomInputDialogState extends State<CustomInputDialog> {
+  User user = boxUser.get("user_key");
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    _textController.text = user.nickname;
+    return AlertDialog(
+      title: Text('Nadimak'),
+      titleTextStyle: paragraph.copyWith(fontWeight: FontWeight.bold),
+      content: TextFormField(
+        controller: _textController,
+        decoration: textInputDecoration,
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(); // Close the dialog
+          },
+          child: Text(
+            'Nazad',
+            style: paragraph.copyWith(fontSize: 12),
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            // Process the user's input
+
+            user.nickname = _textController.text;
+            // Perform actions with inputData
+            // print('User input: $inputData');
+            boxUser.put('user_key', user);
+            Navigator.of(context).pop(); // Close the dialog
+          },
+          child: Text(
+            'Spasi promjene',
+            style: paragraph.copyWith(fontSize: 12),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+}
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:aac/components/input/custom_input_dialog.dart';
 import 'package:aac/components/settings_item.dart';
+import 'package:aac/constants.dart';
 import 'package:aac/pages/add-edit/ud_categories.dart';
 import 'package:aac/pages/settings/accessibility/accessibility_settings.dart';
 import 'package:flutter/material.dart';
@@ -32,8 +34,15 @@ class _SettingsState extends State<Settings> {
             },
           ),
           SettingsItem(
-            name: "Profil",
-            onPressed: () {},
+            name: "Nadimak",
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return CustomInputDialog(); // Your custom input dialog widget
+                },
+              );
+            },
           ),
         ],
       ),
